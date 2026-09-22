b82f57be962335a075f02c4a37fb8e9d4993c9ab
ANDERSON AYORA <ayora.anderson@gmail.com>
2026-09-22 18:55:32 -0300

    feat: implement PgMind core

--- tests/test_retrieval.py
+++ tests/test_retrieval.py
@@ -1,3 +1,2 @@
-from pgmind.retrieval import Record,hybrid
-def test_hybrid_order():
- r=hybrid([Record("sql",1,.2),Record("vec",.2,1)],.5); assert r[0].id=="sql"
+from pgmind.retrieval import *
+def test_hybrid(): assert hybrid([Record('a',1,0),Record('b',0,1)],.5)[0].id=='a'
